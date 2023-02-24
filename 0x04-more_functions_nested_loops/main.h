edd0153fd828baeb8904643db437cdadba038879
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2023-02-23 17:31:30 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - checks if a character c is uppercase or lowercase
+ * @c: character
+ * Return: integer
+ */
+
+int _isupper(int c);
+
+
+
+#endif
