eff4272feacf9cd7225b9b7cfba6f667ec238deb
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2023-02-23 16:34:47 -0800

    push

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,23 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - checks if intfeger i is positive, negative or zero
+ * @i: integer
+ * Return: 0
+ */
+
 void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
+
 
 #endif
