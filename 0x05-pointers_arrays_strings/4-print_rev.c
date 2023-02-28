59513e9163075d78d30e0b7448380a8c4f23d97d
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2023-02-28 04:36:52 -0800

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - prints a string in reverse
@@ -20,7 +21,7 @@ void print_rev(char *s)
 
 	for (i = length; i >= 0; --i)
 	{
-		_putchar(*s);
+		printf("%c",*s);
 		s--;
 	}
 	_putchar('\n');
