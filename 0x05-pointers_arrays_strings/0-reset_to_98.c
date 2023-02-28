3a8430617f9f937bad0f1e4cd73664bf5fa87453
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2023-02-27 16:55:34 -0800

    ./0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * reset_to_98 - updates the value a pointer points to, to an 98
+ * @n: Integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef ALX
+#define ALX
+
+/**
+ * reset_to_98 - updates the value a pointer points to, to 98
+ * @n: Integer
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+
+
+
+#endif
