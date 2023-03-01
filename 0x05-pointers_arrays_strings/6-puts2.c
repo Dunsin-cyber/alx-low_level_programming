c6ccecf86e322f38a679d4b2f66bd034a8de3d6e
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2023-02-28 18:02:41 -0800

    ./6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a striong and a new line
+ * @str: character
+ * Return: void
+ */
+
+
+int _strlen(char *s);
+
+
+void puts2(char *str)
+{
+	int length, i;
+
+	length =  _strlen(str);
+
+	for (i = 0; i < length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	
+	_putchar('\n');
+}
+
+/**
+ * _strlen - counts the length of a string
+ * @s: string to be counted
+ * Return: integer'
+ */
+
+
+
+int  _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
